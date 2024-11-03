302311ef8ab46109e0e11c475f7d6f91956fe7e1
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2024-11-03 22:44:02 +0545

    data base connected and backend get api called

---
+++ Backend/index.js
@@ -0,0 +1,41 @@
+
+import cors from "cors"
+import express from "express"
+import dotenv from "dotenv"
+import mongoose from "mongoose"
+import  bookRoute from "./route/book.route.js"
+
+const app = express()
+app.use(cors())
+
+dotenv.config()
+
+const port =  process.env.PORT|| 4000
+
+app.use(express.json())
+
+
+//connect to moongoDB
+
+
+try {
+    mongoose.connect(process.env.MongoDBURI)
+    console.log("Database connected")
+
+}
+
+
+catch (error) {
+    
+    console.log("Error", error)
+}
+
+// Defining Routes
+
+app.use("/book", bookRoute)
+
+
+app.listen(port, ()=>{
+
+    console.log(`App listening on port ${port}`)
+})
--- Frontend/src/components/Course.jsx
+++ Frontend/src/components/Course.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
 import Cards from './Cards'
 import list from '../../public/list.json'
 import {Link} from 'react-router-dom'
+import axios from "axios"
 
 function Course() {
+ 
+ const [book , setBook] = useState([])
+
+ useEffect(()=>{
+
+const getBook = async ()=>{
+
+try {
+  
+const res = await axios.get("http://localhost:4001/book")
+console.log(res.data)
+setBook(res.data)
+
+} catch (error) {
+  console.log(error)
+}
+}
+getBook()
+
+ },[])
+
   return (
    <>
    
@@ -26,7 +48,7 @@ function Course() {
    <div className='mt-20 grid grid-cols-1 md:grid-cols-3 gap-4'>
  
     {
-           list.map((item)=>(
+           book.map((item)=>(
 
             <Cards key = {item.id} item={ item} />
            ))
