b7116039cc11254dca284c05e176df99baf2a687
dineshgurung123 <134469961+dineshgurung123@users.noreply.github.com>
2024-11-03 11:07:36 +0545

    login form validation done

--- Frontend/src/components/Login.jsx
+++ Frontend/src/components/Login.jsx
@@ -1,19 +1,26 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useForm } from "react-hook-form";
 
 function Login() {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm();
+  
+  const onSubmit = data => console.log(data);
+
   return (
     <>
     
     <div>
 
 
-    <dialog id="my_modal_3" className="modal">
+    <dialog id="my_modal_3" className="modal" >
   <div className="modal-box">
-    <form method="dialog">
+    <form onSubmit={handleSubmit(onSubmit)} method="dialog">
       {/* if there is a button in form, it will close the modal */}
-      <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
-    </form>
+      <Link to="/" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"
+      onClick={()=>document.getElementById("my_model_3").closest()}
+      >✕</Link>
+    
     <h3 className="font-bold text-lg">Login</h3>
    
    {/* email */}
@@ -21,7 +28,12 @@ function Login() {
 
         <span>Email</span>
         <br />
-        <input type="email"  placeholder='Enter email' className='w-80 border rounded-md px-4 outline-none'/>
+        <input type="email"  placeholder='Enter email'
+         className='w-80 border rounded-md px-4 outline-none'
+         {...register("email", { required: true })}
+         />
+         <br/>
+         {errors.email && <span className='text-sm text-red-500'>This field is required</span>}
         <br />
 
     </div>
@@ -32,7 +44,12 @@ function Login() {
 
 <span>Password</span>
 <br />
-<input type="text"  placeholder='Enter email' className='w-80 border rounded-md px-4 outline-none'/>
+<input type="text"  placeholder='Enter email' 
+className='w-80 border rounded-md px-4 outline-none'
+{...register("password", { required: true })}
+/>
+<br />
+{errors.password && <span className='text-sm text-red-500'>This field is required</span>}
 <br />
 
 </div>
@@ -45,7 +62,7 @@ function Login() {
     {" "}
     </p>
 </div>
-
+</form>
   </div>
 </dialog>
     </div>
